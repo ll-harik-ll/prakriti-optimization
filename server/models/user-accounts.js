c99b849760ace11f246943131ddaf481e81ff741
ll-harik-ll <harik2k5@outlook.com>
2025-04-07 11:55:10 +0530

    Model for User Verification

---
+++ server/models/user-accounts.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+// Need to install Bcrypt
+
+const UserSchema = new mongoose.Schema ({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true } // Stored as hashed
+});
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password,10);
+    next();
+});
+
+const Users = mongoose.model('user_accounts', UserSchema, 'user_accounts');
+export default Users;
